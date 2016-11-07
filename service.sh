862c458a098272fa671c0d12d80248859802afa1
Maxim Kornienko <maxim.kornienko@outlook.com>
2016-11-07 03:11:31 +0100

    Service script fix

--- service.sh
+++ service.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Keep the pwd in mind!
 # Example: RUN="java -jar $DIR/target/magic.jar"
-RUN="java -jar $DIR/target/bus-route-challenge.jar $2"
+RUN="java -jar $DIR/target/bus-route-challenge.jar"
 NAME=bus-route-challenge
 
 DATA_FILE=$2
